9d2818e6bb1c3a3778a41fdbfa5dfc6274139802
matheustkaczyk <matheus111904@hotmail.com>
2021-07-06 15:05:39 -0300

    requisito 1 concluido

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
     <div>
       <div>Movie Card Library CRUD</div>
       <BrowserRouter>
-        <Route path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" render={ () => <NewMovie /> } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </BrowserRouter>
     </div>
