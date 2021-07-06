426a3a9fca7cb33a60ba5f0002e7ca09729fc6c6
matheustkaczyk <matheus111904@hotmail.com>
2021-07-06 14:56:40 -0300

    correçoes iniciais lint, inicio requisito 1

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -165,3 +165,8 @@ class MovieForm extends React.Component {
 }
 
 export default MovieForm;
+
+MovieForm.propTypes = {
+  movie: PropTypes.arrayOf(Object).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
